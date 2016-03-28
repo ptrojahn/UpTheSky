6c1d0fc0ec80d47cddd78ed4537c448f2a1b31d1
Paul Trojahn <paul.trojahn@gmail.com>
2016-03-28 22:30:59 +0200

    Reordered transform component constructor arguments
    The arguments are ordered by usage frequency and have default values for quick initialization

--- Layers/include/TransformComponent.h
+++ Layers/include/TransformComponent.h
@@ -6,8 +6,7 @@
 class TransformComponent : public BaseComponent<TransformComponent>
 {
 public:
-	TransformComponent(Vector2<float> scale, float rotation, Vector2<float> position) : scale(scale), position(position), rotation(rotation) {
-
+	TransformComponent(Vector2<float> position = Vector2<float>(0, 0), float rotation = 0, Vector2<float> scale = Vector2<float>(1, 1)) : scale(scale), position(position), rotation(rotation) {
 	}
 	Vector2<float> scale;
 	Vector2<float> position;
