e9381ea0c38ea28d9edc1632f5b4f35f476dc959
Paul Trojahn <paul.trojahn@gmail.com>
2016-03-31 03:51:29 +0200

    Android support
    Added android makefile
    Changed wmain to main
    Changed subsystem to console

--- WallJump/main.cpp
+++ WallJump/main.cpp
@@ -1,7 +1,10 @@
 #include "LayersEngine.h"
 
-int wmain(int argc, wchar_t* argv[]) {
+#include "glFunctions.h"
+
+int main(int argc, char* argv[]) {
 	LayersEngine engine(9, 16);
+	glClearColor(0.643, 0.776, 0.224, 1);
 	engine.run();
 	return 0;
 }
