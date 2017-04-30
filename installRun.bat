9c715644b8b5c0e7e6d9efbcb3e8840ef946b1f0
Paul Trojahn <paul.trojahn@gmail.com>
2017-05-01 01:23:47 +0200

    Migrated Project to Android Studio
    Replaced make with cmake.
    Added Android Studio project.

--- installRun.bat
+++
@@ -1,3 +0,0 @@
-call adb push bin/UpTheSky-debug.apk /sdcard/
-call adb shell pm install -r /sdcard/UpTheSky-debug.apk
-call adb shell am start -n de.paultrojahn.UpTheSky/de.paultrojahn.UpTheSky.UpTheSkyActivity
--- jni/Application.mk
+++
@@ -1,9 +0,0 @@
-
-# Uncomment this if you're using STL in your project
-# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
-NDK_TOOLCHAIN_VERSION := clang
-APP_PLATFORM := android-9
-APP_STL := c++_shared
-APP_CPPFLAGS += -std=c++11 -frtti
-APP_ABI := armeabi-v7a
-#x86 armeabi
